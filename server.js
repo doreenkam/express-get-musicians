20131b059eb9c86b5541e2ebe91438db37e1918e
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-20 12:47:43 +0000

    Finished task'

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const { Musician } = require('./Musician');
+const { sequelize } = require('./db');
 
 const port = 3000;
 
 //TODO
+app.get('/musicians', async (req, res) => {
+  const data = await Musician.findAll();
+  res.json(data);
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`);
+});
